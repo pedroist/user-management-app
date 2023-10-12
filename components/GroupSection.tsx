f48d6aa098482f4d92957712fb9bb51d2bf76c22
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-10-12 15:46:32 +0100

    Created Groups page. Users still not being filtered by group

---
+++ components/GroupSection.tsx
@@ -0,0 +1,88 @@
+import React, { FC, useCallback } from 'react'
+import {UserInput } from '../interfaces/user'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Tag,
+  IconButton,
+  useDisclosure,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react'
+import { FiMoreVertical } from 'react-icons/fi';
+import classes from './UserTable.module.scss'
+import { useUsers } from '../context/userContext'
+
+
+const GroupSection: FC = () => {
+  const { users, deleteUser } = useUsers();
+  
+  return(
+    <>
+      <TableContainer>
+        <Table variant='simple'>
+          <TableCaption>List of users of a specific group and respective details</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Email</Th>
+              <Th>Type</Th>
+              <Th>Groups</Th>
+              <Th>Age</Th>
+              <Th>Location</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {(users || []).map((user) => (
+              <Tr key={user.id}>
+                <Td>{user.name}</Td>
+                <Td>{user.email}</Td>
+                <Td>
+                  <Tag size={'md'} variant='solid' colorScheme='green'>
+                    {user.type}
+                  </Tag>
+                </Td>
+                <Td>
+                  {(user.groups || []).map((group, index) => (
+                    <Tag key={index} size={'md'} variant='solid' colorScheme='blue' mr='10px'>
+                      {group}
+                    </Tag>
+                  ))}
+                </Td>
+                <Td>{user.age}</Td>
+                <Td>{user.location}</Td>
+                <Td>
+                  <Menu>
+                    <MenuButton className={classes.menuButton} as={IconButton}
+                      aria-label='actions'
+                      size='sm'
+                      variant='ghost'
+                      colorScheme='gray'
+                      icon={<FiMoreVertical/>}
+                      isRound
+                    />
+                    <MenuList>
+                      <MenuItem>Add to a Group</MenuItem>
+                      <MenuItem onClick={() => deleteUser(user.id)}>Delete</MenuItem>
+                    </MenuList>
+                  </Menu>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+  )
+}
+
+export default GroupSection
---
+++ pages/groups.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next'
+import Layout from '../components/Layout'
+import { Box, Heading } from '@chakra-ui/react'
+import GroupSection from '../components/GroupSection'
+
+
+const GroupsPage: NextPage = () => (
+  <Layout title="Groups List">
+    <Heading>Groups List</Heading>
+    <Box my="40px">
+      <GroupSection/>
+    </Box>
+  </Layout>
+)
+
+export default GroupsPage
